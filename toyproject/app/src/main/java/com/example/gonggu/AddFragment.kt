6f5c1a9f20109953764105ab95c9a63e60347889
zhenying2 <khappy9888@sookmyung.ac.kr>
2021-12-22 18:47:34 +0900

    [feat] bottom navigation 추가

--- toyproject/app/src/main/java/com/example/gonggu/AddFragment.kt
+++ toyproject/app/src/main/java/com/example/gonggu/AddFragment.kt
@@ -1,6 +1,9 @@
 package com.example.gonggu
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.gonggu.databinding.FragmentAddPostBinding
 import com.example.gonggu.retrofit.RetrofitManager
@@ -10,12 +13,12 @@ class AddFragment : Fragment(){
     private var mBinding: FragmentAddPostBinding? = null
     private val binding get() = mBinding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        //레이아웃과 연결
-        mBinding = FragmentAddPostBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding= FragmentAddPostBinding.inflate(inflater, container, false)
 
         //공구 등록하기 버튼이 클릭되었을 때
         binding.createGoggu.setOnClickListener{
@@ -28,11 +31,13 @@ class AddFragment : Fragment(){
             //var price=binding.etPrice.text.toString()
             RetrofitManager.instance.write(title,description,link,contact,date)
         }
+
+        return binding.root
     }
 
-    //액티비티 파괴될 때 binding class 인스턴스 참조 정리
-    override fun onDestroy() {
+
+    override fun onDestroyView() {
+        super.onDestroyView()
         mBinding=null
-        super.onDestroy()
     }
 }
--- toyproject/app/src/main/java/com/example/gonggu/MainActivity.kt
+++ toyproject/app/src/main/java/com/example/gonggu/MainActivity.kt
@@ -14,20 +14,7 @@ class MainActivity: AppCompatActivity(), BottomNavigationView.OnNavigationItemSe
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //test recycler view
-        val MainItemList= arrayListOf(
-            MainItem("글제목1", "저자", "공구링크", "공구가격", "공구기간"),
-            MainItem("글제목2", "저자", "공구링크", "공구가격", "공구기간"),
-            MainItem("글제목3", "저자", "공구링크", "공구가격", "공구기간"),
-            MainItem("글제목4", "저자", "공구링크", "공구가격", "공구기간")
-        )
-
-        rv_items.layoutManager= LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
-        rv_items.setHasFixedSize(true)
-
-        rv_items.adapter= MainItemAdapter(MainItemList)
-
+        
         //bottom navigation
         bottom_navigationbar.setOnNavigationItemSelectedListener(this)
         //set default screen
@@ -39,22 +26,23 @@ class MainActivity: AppCompatActivity(), BottomNavigationView.OnNavigationItemSe
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
         when(p0.itemId){
             R.id.action_home->{
-                startActivity(Intent(this, MainActivity::class.java))
+                var MainDetailFragment = MainDetailFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,MainDetailFragment).commit()
                 return true
             }
             R.id.action_favorite->{
-                startActivity(Intent(this, FavoriteActivity::class.java))
-                finish()
+                var favoriteFragment = FavoriteFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,favoriteFragment).commit()
                 return true
             }
             R.id.action_add->{
-                startActivity(Intent(this, AddActivity::class.java))
-                finish()
+                var AddFragment = AddFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,AddFragment).commit()
                 return true
             }
             R.id.action_mypage->{
-                startActivity(Intent(this, MypageActivity::class.java))
-                finish()
+                var MypageFragment = MypageFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,MypageFragment).commit()
                 return true
             }
         }
